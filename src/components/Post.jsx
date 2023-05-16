cde1d6b8045443c4a41fdd7d0096c1168de92899
EllenBessa <ellenbessa96@gmail.com>
2023-05-16 13:21:28 -0300

    feat: :lipstick: Estrutura da aplicação - Estilizações do Post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -29,11 +29,14 @@ export function Post() {
         </p>
 
         <p>
-          <a href="#"> 👉 jane.design/doctorcare</a>
+          <a href="#"> 👉{""} jane.design/doctorcare</a>
         </p>
 
         <p>
-          <a href="#"> #novoprojeto #nlw #rocketseat</a>
+          <a href="#"> #novoprojeto</a> {""}
+          <a href="#"> #nlw</a>
+          {""}
+          <a href="#"> #rocketseat</a>
         </p>
       </div>
     </article>
