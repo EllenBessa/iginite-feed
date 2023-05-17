83f41835312e8c2f604feb4fe5804ce0ec2853b5
EllenBessa <ellenbessa96@gmail.com>
2023-05-16 21:25:58 -0300

    feat: :lipstick: Estrutura da aplicação - Componente: Avatar

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,10 +1,16 @@
 import { ThumbsUp, Trash } from "phosphor-react";
+import { Avatar } from "./Avatar";
+
 import styles from "./Comment.module.css";
 
 export function Comment() {
   return (
     <div className={styles.comment}>
-      <img src="https://github.com/ellenbessa.png" alt="" />
+      <Avatar
+        hasBorder={false}
+        src="https://github.com/ellenbessa.png"
+        alt=""
+      />
 
       <div className={styles.commentBox}>
         <div className={styles.commentContent}>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilLine } from "phosphor-react";
+import { Avatar } from "./Avatar";
 
 import styles from "./Sidebar.module.css";
 
@@ -12,11 +13,7 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
-        <img
-          className={styles.avatar}
-          src="https://github.com/ellenbessa.png"
-          alt=""
-        />
+        <Avatar src="https://github.com/ellenbessa.png" />
 
         <strong>Ellen Bessa</strong>
         <span>Web Developer</span>
